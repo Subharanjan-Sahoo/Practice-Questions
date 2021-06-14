b69b31249b360377a715962e10997e9bce473100
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-06-14 21:01:14 +0530

    Problem 3

---
+++ Problem_3.py
@@ -0,0 +1,21 @@
+# You have given an array of integers a and an integer k. 
+# Your task to calculate the number of way to pick two diffrerent indices i < j,
+# such that a[i]+ a[j] is divisible by k.
+
+
+def sumDivisibleBy(a ,k):
+    num = 0
+    sum = 0
+    for i in range(len(a)):
+        for j in range(len(a)):
+                sum = a[i] + a[j]
+                if i< j:
+                    #print(a[i], a[j])
+                    #print(sum)
+                    if (sum % k == 0):
+                         num = num +1
+    print(num)
+
+s = sumDivisibleBy([1, 2, 3, 4, 5],3)
+#print(sumDivisibleBy(a ,k))
+
