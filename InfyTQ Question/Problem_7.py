2c337fa012d1b6a3d601b3e21e90f060f54c6af6
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-07-17 20:12:59 +0530

    Problem_7.py

---
+++ InfyTQ Question/Problem_7.py
@@ -0,0 +1,13 @@
+def Candies(sold):
+    if sold >= 5 :
+        print("Invalid Input")
+    elif sold == 0 :
+        print("Invalid Input")
+    else :
+        print("Number of Candies sold: ", sold)
+        print("Number of Candies Available : " , 10 - sold)
+
+
+
+num = int(input("Enter the number of candies are sold: "))
+Candies(num)
