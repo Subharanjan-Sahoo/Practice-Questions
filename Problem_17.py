60fc7c787179e1586392931396c227c585e2f560
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-08-02 20:46:03 +0530

    Problem 17

---
+++ Problem_17.py
@@ -0,0 +1,15 @@
+def minipenalty(input1):
+    input2 = list(map(int, input("Enter the elements: ").strip().split(",")))
+    temp = input2[::-1]
+    for i in range(input1):
+        for j in range(input1):
+           
+
+
+
+
+
+
+
+input1 = int(input("Enter the number: "))
+minipenalty(input1)
