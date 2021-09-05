89ed61f1794a1befacd81574aaadca01e64d137b
Subharanjan-Sahoo <subhasahoo881@gmail.com>
2021-09-05 20:43:44 +0530

    Problem 34

---
+++ Problem_34.py
@@ -0,0 +1,43 @@
+'''
+Single File Programming Question
+
+Your little brother has a math assignment to find whether the given number is a power of 2. If it is a power of 2 then he has to find the sum of the digits. If it is not a power of 2, then he has to find the next number which is a power of 2. He asks for your help to validate his work. But you are already busy playing video games. Develop a program so that your brother can validate his work by himself.
+
+Example 1
+
+Input
+
+64
+
+Output
+
+Yes
+
+10
+
+Example 2
+
+Input
+
+133
+
+Output
+
+NO
+
+
+'''
+
+def math(input1):
+    if int(input1) % 2 == 0:
+        sumyes = 0
+        print('Yes')
+        for i in range(len(input1)):
+            sumyes = int(input1[i]) + sumyes
+        print(sumyes)    
+    else:
+        print('NO')
+
+
+input1 = input()
+math(input1)
